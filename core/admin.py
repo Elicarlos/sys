73630984973acbf731771afae06ede81c1accd71
Elicarlos <elicarlosantos_@hotmail.com>
2018-09-16 21:43:14 -0300

    Ajuste de models

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Cliente, Atendimento, Tecnico, Sistemas
 
 # Register your models here.
+
+admin.site.register(Cliente)
+admin.site.register(Atendimento)
+admin.site.register(Tecnico)
+admin.site.register(Sistemas)
--- core/models.py
+++ core/models.py
@@ -3,24 +3,40 @@ from django.utils import timezone
 
 # Create your models here.
 class Cliente(models.Model):
-    codigo = models.ForeingKey('auth.User', on_delete=models.CASCADE)
     cnpj = models.CharField(max_length=30)
     razao = models.CharField(max_length=200)
     fantazia = models.CharField(max_length=200)
-    sistema = models.CharField(max_length=30)
     contato = models.CharField(max_length=30)
 
+    def __str__(self):
+        return self.fantazia
 
 
+class Sistemas(models.Model):
+    nome = models.CharField(max_length=50)
 
-
-class Atendimento(models.Model):
-    numero = models.
-    data_ini =
-    data_fim
-    reclamado =
+    def __str__(self):
+        return self.nome
 
 
 class Tecnico(models.Model):
-    codigo = 
     nome = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.nome
+
+class Atendimento(models.Model):
+    numero = models.AutoField(primary_key=True)
+    nomeFantazia = models.ForeignKey(Cliente, on_delete=models.CASCADE)
+    data_ini = models.DateTimeField(auto_now_add=True)
+    data_fim = models.DateTimeField(blank=True, null=True)
+    reclamado = models.TextField()
+    sistema = models.ForeignKey(Sistemas, on_delete=models.CASCADE)
+    tecnico = models.ForeignKey(Tecnico, on_delete=models.CASCADE)
+
+    def __str__(self):
+       return str(self.numero)
+
+    def valida(self):
+        self.data_ini = timezone.now()
+        self.save()
